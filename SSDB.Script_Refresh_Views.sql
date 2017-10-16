145b32fd3eefb05ae85a11c2d68fbb7384ad424a
Anthony <anthonyduguid@gmail.com>
2017-10-16 15:25:44 +1100

    Update SSDB.Script_Refresh_Views.sql

--- SSDB.Script_Refresh_Views.sql
+++ SSDB.Script_Refresh_Views.sql
@@ -1,8 +1,10 @@
 SELECT DISTINCT 
-'EXEC sp_refreshview ''' + sc.name + '.' + so.name + '''' AS IndividualScript
-FROM sys.objects AS so 
-INNER JOIN sys.schemas sc ON sc.schema_id = so.schema_id
-INNER JOIN sys.sql_dependencies AS sed 
-    ON so.object_id = sed.object_id
-WHERE       so.type = 'V' 
---AND OBJECT_NAME(sed.referenced_major_id) LIKE 'MyTable%'
+	IndividualScript = 'EXEC sp_refreshview ''' + sc.name + '.' + so.name + ''''
+FROM 
+	sys.objects AS so 
+	INNER JOIN sys.schemas sc ON sc.schema_id = so.schema_id
+	INNER JOIN sys.sql_dependencies AS sed ON so.object_id = sed.object_id
+WHERE       
+	1=1
+	AND so.type = 'V' 
+	--AND OBJECT_NAME(sed.referenced_major_id) LIKE 'MyTable%'
